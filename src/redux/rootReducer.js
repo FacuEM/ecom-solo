631523c6d2a5c415551b3b86d16940209b1e488d
Facundo Martinez <ifloyd@MacBook-Air-de-Facundo.local>
2021-02-17 10:53:47 -0300

    redux-persist added

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,21 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import productsReducer from "./Product/products.reducers";
 import userReducer from "./User/user.reducer";
 import cartReducer from "./Cart/cart.reducers";
 
-export default combineReducers({
+export const rootReducer = combineReducers({
   user: userReducer,
   productsData: productsReducer,
   cartData: cartReducer,
 });
+
+const configStorage = {
+  key: "root",
+  storage,
+  whitelist: ["cartData"],
+};
+
+export default persistReducer(configStorage, rootReducer);
